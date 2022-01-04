f646e3bcbcb04c8e3f10c8a1d1d178a0b010bcae
bcssov <nxsovereign@gmail.com>
2022-01-04 17:16:14 +0100

    Add initial load and rough display logic

--- DynamicNameGenerator/DynamicNameGenerator/MainWindow.xaml.cs
+++ DynamicNameGenerator/DynamicNameGenerator/MainWindow.xaml.cs
@@ -1,17 +1,25 @@
-﻿using System;
+﻿// ***********************************************************************
+// Assembly         : DynamicNameGenerator
+// Author           : Mario
+// Created          : 01-04-2022
+//
+// Last Modified By : Mario
+// Last Modified On : 01-04-2022
+// ***********************************************************************
+// <copyright file="MainWindow.xaml.cs" company="Mario">
+//     Mario
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DynamicNameGenerator
 {
@@ -20,9 +28,103 @@ namespace DynamicNameGenerator
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Fields
+
+        /// <summary>
+        /// The grid data
+        /// </summary>
+        private ObservableCollection<MainData> gridData = new ObservableCollection<MainData>();
+
+        /// <summary>
+        /// The grid view source
+        /// </summary>
+        private CollectionViewSource gridViewSource;
+
+        /// <summary>
+        /// The visible items
+        /// </summary>
+        private int visibleItems = 0;
+
+        #endregion Fields
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+            Style = (Style)FindResource(typeof(Window));
+            DataContext = this;
+            InitializeGrid();
         }
+
+        #endregion Constructors
+
+        #region Methods
+
+        /// <summary>
+        /// Handles the AddingNewItem event of the dataGrid control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="AddingNewItemEventArgs"/> instance containing the event data.</param>
+        private void dataGrid_AddingNewItem(object sender, AddingNewItemEventArgs e)
+        {
+            filter.Text = string.Empty;
+        }
+
+        /// <summary>
+        /// Handles the LoadingRow event of the DataGrid control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="DataGridRowEventArgs"/> instance containing the event data.</param>
+        private void DataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            if (e.Row.GetIndex() < visibleItems)
+            {
+                e.Row.Header = (e.Row.GetIndex() + 1).ToString();
+            }
+            else
+            {
+                e.Row.Header = string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the grid.
+        /// </summary>
+        private void InitializeGrid()
+        {
+            var json = File.ReadAllText(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data.json"));
+            gridData = new ObservableCollection<MainData>(JsonConvert.DeserializeObject<List<MainData>>(json) ?? new List<MainData>());
+
+            gridViewSource = new CollectionViewSource() { Source = gridData };
+            gridViewSource.View.Filter = null;
+            visibleItems = gridData.Count;
+            dataGrid.ItemsSource = gridViewSource.View;
+            filter.Text = string.Empty;
+            gridViewSource.View.CollectionChanged += View_CollectionChanged;
+        }
+
+        /// <summary>
+        /// Handles the TextChanged event of the TextBox control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.Windows.Controls.TextChangedEventArgs"/> instance containing the event data.</param>
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Handles the CollectionChanged event of the View control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.Collections.Specialized.NotifyCollectionChangedEventArgs"/> instance containing the event data.</param>
+        private void View_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+        }
+
+        #endregion Methods
     }
 }
---
+++ DynamicNameGenerator/DynamicNameGenerator/Models.cs
@@ -0,0 +1,180 @@
+﻿// ***********************************************************************
+// Assembly         : DynamicNameGenerator
+// Author           : Mario
+// Created          : 01-04-2022
+//
+// Last Modified By : Mario
+// Last Modified On : 01-04-2022
+// ***********************************************************************
+// <copyright file="Models.cs" company="Mario">
+//     Mario
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace DynamicNameGenerator
+{
+    /// <summary>
+    /// Class MainData.
+    /// Implements the <see cref="System.ComponentModel.INotifyPropertyChanged" />
+    /// </summary>
+    /// <seealso cref="System.ComponentModel.INotifyPropertyChanged" />
+    public class MainData : INotifyPropertyChanged
+    {
+        #region Fields
+
+        /// <summary>
+        /// The provinces
+        /// </summary>
+        private List<ProvinceData> provinces = new List<ProvinceData>();
+
+        /// <summary>
+        /// The state identifier
+        /// </summary>
+        private string stateId = string.Empty;
+
+        /// <summary>
+        /// The type
+        /// </summary>
+        private string type = string.Empty;
+
+        #endregion Fields
+
+        #region Events
+
+        /// <summary>
+        /// Occurs when [property changed].
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion Events
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the provinces.
+        /// </summary>
+        /// <value>The provinces.</value>
+        public List<ProvinceData> Provinces
+        {
+            get
+            {
+                return provinces;
+            }
+            set
+            {
+                provinces = value ?? new List<ProvinceData>();
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Provinces)));
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the state identifier.
+        /// </summary>
+        /// <value>The state identifier.</value>
+        public string StateId
+        {
+            get
+            {
+                return stateId;
+            }
+            set
+            {
+                stateId = value ?? string.Empty;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(StateId)));
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
+        /// <value>The type.</value>
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                type = value ?? string.Empty;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Type)));
+            }
+        }
+
+        #endregion Properties
+    }
+
+    /// <summary>
+    /// Class ProvinceData.
+    /// Implements the <see cref="System.ComponentModel.INotifyPropertyChanged" />
+    /// </summary>
+    /// <seealso cref="System.ComponentModel.INotifyPropertyChanged" />
+    public class ProvinceData : INotifyPropertyChanged
+    {
+        #region Fields
+
+        /// <summary>
+        /// The identifier
+        /// </summary>
+        private string id = string.Empty;
+
+        /// <summary>
+        /// The name
+        /// </summary>
+        private string name = string.Empty;
+
+        #endregion Fields
+
+        #region Events
+
+        /// <summary>
+        /// Occurs when [property changed].
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion Events
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value ?? string.Empty;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Id)));
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value ?? string.Empty;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+            }
+        }
+
+        #endregion Properties
+    }
+}
